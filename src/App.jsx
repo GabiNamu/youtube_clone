0d4881aacc6077d46877fa43569c766c37fbe03c
Gabriela Muniz <gabrielanamu@live.com>
2023-01-09 18:59:43 -0300

    add recomended part

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css';
 import Header from './components/Header';
+import RecomendedVideos from './components/RecomendedVideos';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from "react-router"
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Routes>
+       <Route path="/" 
+       element={ 
+       <>
+       <Header />
+       <div className="app__page">
+        <Sidebar />
+        <RecomendedVideos />
+       </div>
+       </> 
+       } />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/RecomendedVideos.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import '../recomendedVideos.css';
+import VideoCard from './VideoCard';
+
+function RecomendedVideos() {
+  return (
+    <div className='recomended-videos'>
+       <h2>Recomended</h2> 
+       <div className="recomendedVideos__videos">
+        <VideoCard 
+        title="day in the life of a software engineer in NYC *an unglamorous 24 hours with meee*"
+        views="1,9mi views"
+        timestamp="7 days ago"
+        channelImage="https://yt3.googleusercontent.com/ytc/AMLnZu9tokLwOzwwjndRwK5iqkwAY_vpWxpH9BdB_iCxwQ=s900-c-k-c0x00ffffff-no-rj"
+        channel="sarah pan"
+        image="https://i.ytimg.com/vi/O62kAvoO9lM/maxresdefault.jpg"
+        />
+
+<VideoCard 
+        title="day in the life of a software engineer in NYC *an unglamorous 24 hours with meee*"
+        views="1,9mi views"
+        timestamp="7 days ago"
+        channelImage="https://yt3.googleusercontent.com/ytc/AMLnZu9tokLwOzwwjndRwK5iqkwAY_vpWxpH9BdB_iCxwQ=s900-c-k-c0x00ffffff-no-rj"
+        channel="sarah pan"
+        image="https://i.ytimg.com/vi/O62kAvoO9lM/maxresdefault.jpg"
+        />
+
+<VideoCard 
+        title="day in the life of a software engineer in NYC *an unglamorous 24 hours with meee*"
+        views="1,9mi views"
+        timestamp="7 days ago"
+        channelImage="https://yt3.googleusercontent.com/ytc/AMLnZu9tokLwOzwwjndRwK5iqkwAY_vpWxpH9BdB_iCxwQ=s900-c-k-c0x00ffffff-no-rj"
+        channel="sarah pan"
+        image="https://i.ytimg.com/vi/O62kAvoO9lM/maxresdefault.jpg"
+        />
+
+<VideoCard 
+        title="day in the life of a software engineer in NYC *an unglamorous 24 hours with meee*"
+        views="1,9mi views"
+        timestamp="7 days ago"
+        channelImage="https://yt3.googleusercontent.com/ytc/AMLnZu9tokLwOzwwjndRwK5iqkwAY_vpWxpH9BdB_iCxwQ=s900-c-k-c0x00ffffff-no-rj"
+        channel="sarah pan"
+        image="https://i.ytimg.com/vi/O62kAvoO9lM/maxresdefault.jpg"
+        />
+
+<VideoCard 
+        title="day in the life of a software engineer in NYC *an unglamorous 24 hours with meee*"
+        views="1,9mi views"
+        timestamp="7 days ago"
+        channelImage="https://yt3.googleusercontent.com/ytc/AMLnZu9tokLwOzwwjndRwK5iqkwAY_vpWxpH9BdB_iCxwQ=s900-c-k-c0x00ffffff-no-rj"
+        channel="sarah pan"
+        image="https://i.ytimg.com/vi/O62kAvoO9lM/maxresdefault.jpg"
+        />
+
+<VideoCard 
+        title="day in the life of a software engineer in NYC *an unglamorous 24 hours with meee*"
+        views="1,9mi views"
+        timestamp="7 days ago"
+        channelImage="https://yt3.googleusercontent.com/ytc/AMLnZu9tokLwOzwwjndRwK5iqkwAY_vpWxpH9BdB_iCxwQ=s900-c-k-c0x00ffffff-no-rj"
+        channel="sarah pan"
+        image="https://i.ytimg.com/vi/O62kAvoO9lM/maxresdefault.jpg"
+        />
+
+<VideoCard 
+        title="day in the life of a software engineer in NYC *an unglamorous 24 hours with meee*"
+        views="1,9mi views"
+        timestamp="7 days ago"
+        channelImage="https://yt3.googleusercontent.com/ytc/AMLnZu9tokLwOzwwjndRwK5iqkwAY_vpWxpH9BdB_iCxwQ=s900-c-k-c0x00ffffff-no-rj"
+        channel="sarah pan"
+        image="https://i.ytimg.com/vi/O62kAvoO9lM/maxresdefault.jpg"
+        />
+       </div>
+    </div>
+  )
+}
+
+export default RecomendedVideos
